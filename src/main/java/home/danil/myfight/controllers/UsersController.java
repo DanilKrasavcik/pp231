908fe0a02e5c682d4468808466b55d987e29823f
DanilKrasavcik <dkrasavcik91@gmail.com>
2023-05-15 15:05:04 +0300

    hz1 commit

---
+++ src/main/java/home/danil/myfight/controllers/UsersController.java
@@ -0,0 +1,75 @@
+package home.danil.myfight.controllers;
+
+import home.danil.myfight.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import home.danil.myfight.models.User;
+
+import javax.validation.Valid;
+
+/**
+ * @author Neil Alishev
+ */
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+
+    private final UsersService usersService;
+
+    @Autowired
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("users", usersService.findAll());
+        return "users/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", usersService.findOne(id));
+        return "users/show";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "users/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("user") @Valid User user,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "users/new";
+
+        usersService.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", usersService.findOne(id));
+        return "users/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "users/edit";
+
+        usersService.update(id, user);
+        return "redirect:/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        usersService.delete(id);
+        return "redirect:/users";
+    }
+}
---
+++ src/main/java/home/danil/myfight/repositories/UsersRepository.java
@@ -0,0 +1,9 @@
+package home.danil.myfight.repositories;
+
+import home.danil.myfight.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<User, Integer> {
+}
